771ca5ce4318f77cf2d34378c2aa566cc66f7b27
audreydk <audreydk@cs.washington.edu>
2024-05-11 15:37:37 -0700

    get navigation controller functionality to work

--- iQuiz/iQuiz/ViewController.swift
+++ iQuiz/iQuiz/ViewController.swift
@@ -14,6 +14,12 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        // self is whole class instance of ViewController
+        // By setting tableView.delegate to self, you're essentially saying, "Hey tableView, I will
+        // handle your delegate methods and interactions, so use me as your delegate." This allows
+        // you to implement methods from the UITableViewDelegate protocol within the same class or
+        // struct where you've set the delegate, enabling you to customize the behavior of the
+        // table view as needed.
         tableView.dataSource = self
         tableView.delegate = self
     }
@@ -37,6 +43,13 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         return cell
     }
     
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let vc = storyboard.instantiateViewController(withIdentifier: "quiz")
+        vc.navigationItem.title = topics[indexPath.row]
+        navigationController?.pushViewController(vc, animated: true) // this allows our view to change to our view controller for the quiz we clicked on
+        
+    }
     
 }
 
