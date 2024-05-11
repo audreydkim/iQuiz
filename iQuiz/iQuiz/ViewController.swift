caf8e5309a0a0d8ccfe58dc08a8db06bff3e9057
audreydk <audreydk@cs.washington.edu>
2024-05-10 20:19:58 -0700

    establish basic tableView and cells

--- iQuiz/iQuiz/ViewController.swift
+++ iQuiz/iQuiz/ViewController.swift
@@ -7,13 +7,36 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    let topics = ["Mathematics", "Marvel Super Heroes", "Science"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        tableView.dataSource = self
+        tableView.delegate = self
     }
 
-
+    @IBOutlet weak var tableView: UITableView!
+    
+    // MARK: - Table View Delegate and Data Source
+    
+    // Establish number of sections
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return topics.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel!.text = topics[indexPath.row]
+        return cell
+    }
+    
+    
 }
 
