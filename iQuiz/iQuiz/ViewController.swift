31cfae9cc346ab766f02605658c68d31ef108c97
audreydk <audreydk@cs.washington.edu>
2024-05-20 15:23:09 -0700

    figured out fetching data and displaying

--- iQuiz/iQuiz/ViewController.swift
+++ iQuiz/iQuiz/ViewController.swift
@@ -7,6 +7,18 @@
 
 import UIKit
 
+struct Quiz : Codable  {
+    var title: String
+    var desc: String
+    var questions: [Question]
+}
+
+struct Question : Codable {
+    var text: String
+    var answer: String
+    var answers: [String]
+}
+
 class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     // MARK: - Variables
@@ -17,6 +29,7 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         "How well do you know your Marvel Super Heroes? Test your knowledge here on our Marvel Super Hero Quiz!",
         "Are you a Science nerd? Test your knowledge on our Science Quiz!"
     ]
+    let fileName = "myFile.json"
     
     // MARK: - UI Components
     @IBOutlet weak var tableView: UITableView!
@@ -31,11 +44,8 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        title = "Quizzes"
         let nib = UINib(nibName: "customCell", bundle: nil)
         tableView.register(nib, forCellReuseIdentifier: customCell.identifier)
-        let nib2 = UINib(nibName: "QuizViewController", bundle: nil)
-        tableView.register(nib2, forCellReuseIdentifier: QuizViewController.identifier)
         // self is whole class instance of ViewController
         // By setting tableView.delegate to self, you're essentially saying, "Hey tableView, I will
         // handle your delegate methods and interactions, so use me as your delegate." This allows
@@ -44,10 +54,8 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         // table view as needed.
         tableView.dataSource = self
         tableView.delegate = self
-        
-        // Settings Button
-//        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(openSettings))
-        
+        title = "Quizzes"
+        fetch()
     }
     
     // MARK: - Table View Delegate and Data Source
@@ -58,7 +66,8 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return topics.count
+        let data = getData()
+        return data.count
     }
     
     // This method is called by the table view when it needs to display a cell for a specific row.
@@ -67,20 +76,112 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         // This line dequeues a reusable cell from the table view's reuse queue. It uses the identifier provided by
         // customCell.identifier to dequeue a cell of the custom class customCell.
         let cell = tableView.dequeueReusableCell(withIdentifier: customCell.identifier, for: indexPath) as! customCell // makes cell be of type customCell custom class that we made, if we don't do this cell will just use generic class that doesn't have the cell.title etc that we would want
-        
-        cell.title.text = topics[indexPath.row]
+        let data = getData()
+        cell.title.text = data[indexPath.row].title
         cell.icon.image = UIImage(named: icons[indexPath.row])
-        cell.descr.text = descriptions[indexPath.row]
+        cell.descr.text = data[indexPath.row].desc
         return cell
     }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        tableView.deselectRow(at: indexPath, animated: true)        
+        tableView.deselectRow(at: indexPath, animated: true)
         if let vc = storyboard?.instantiateViewController(withIdentifier: "QuizViewController") as? QuizViewController {
             navigationController?.pushViewController(vc, animated: true) // this allows our view to change to our view controller for the quiz we clicked on
         }
-         
     }
     
+    // ---------------------------------------------------
+    func fetch() {
+        NSLog("We fetching")
+        DispatchQueue.global().sync {
+            NSLog("Inside global().async")
+            
+            // Issue a GET to https://tednewardsandbox.site44.com/questions.json
+            let url = URL(string: "https://tednewardsandbox.site44.com/questions.json")!
+            let task = URLSession.shared.dataTask(with: url) { data, response, error in
+                if let error = error {
+                    NSLog("\(error)")
+                } else {
+                    NSLog("We have no error")
+                }
+                //This Swift code is using a guard statement to check whether the response from a network request is an HTTPURLResponse with a status code indicating success (status code between 200 and 299, inclusive) or is nil as in unsuccessful.
+                guard let httpResponse = response as? HTTPURLResponse, (200...299).contains(httpResponse.statusCode) else {
+                    NSLog(("Error!"))
+                    return // end of fetch function bc there was an error
+                }
+                
+                let string = String(data: data!, encoding: .utf8)!
+                NSLog("Data returned is \(string)")
+                NSLog("Data type of data: \(type(of: data))")
+                
+                // This call saves the data from the json we fetched from the internet to a local
+                // directory
+                self.saveData(data!)
+            }
+            task.resume()
+            NSLog("We Fetched")
+            
+        }
+    }
+    
+//    func saveData(_ data: Data) {
+//        do {
+//            // Create's and writes to File
+//            let fileManager = FileManager.default
+//            let documentDirectory = fileManager.urls(for: .documentDirectory, in: .userDomainMask).first!
+//            let filesInDoc = try fileManager.contentsOfDirectory(at: documentDirectory, includingPropertiesForKeys: nil)
+//            NSLog("my curr files in document directory: \(filesInDoc)")
+//            let fileURL = documentDirectory.appendingPathComponent(fileName)
+//            let jsonEncoder = JSONEncoder()
+//            let jsonData = try jsonEncoder.encode(data)
+//            try jsonData.write(to: fileURL)
+//        } catch {
+//            NSLog("Error when saving data")
+//        }
+//    }
+    func saveData(_ data: Data) {
+        do {
+            let fileManager = FileManager.default
+            let documentDirectory = fileManager.urls(for: .documentDirectory, in: .userDomainMask).first!
+            let fileURL = documentDirectory.appendingPathComponent(fileName)
+            
+            // Check if file exists and handle the case appropriately
+            if fileManager.fileExists(atPath: fileURL.path) {
+                try fileManager.removeItem(at: fileURL)
+            }
+            try data.write(to: fileURL)
+        } catch {
+            NSLog("Error when saving data: \(error)")
+        }
+        
+    }
+    
+//    func getData() -> Array<Quiz> {
+//        do {
+//            let fileDirectory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first
+//            let fileURL = URL(fileURLWithPath: fileName, relativeTo: fileDirectory)
+//            let jsonData = try Data(contentsOf: fileURL)
+//            let decoder = JSONDecoder()
+//            return try decoder.decode(Array<Quiz>.self, from: jsonData)
+//        } catch {
+//            NSLog("Error in getting data from local json file")
+//        }
+//        NSLog("err")
+//    }
+    
+    func getData() -> [Quiz] {
+        do {
+            let fileDirectory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first
+            let fileURL = URL(fileURLWithPath: fileName, relativeTo: fileDirectory)
+            let jsonData = try Data(contentsOf: fileURL)
+            let decoder = JSONDecoder()
+            return try decoder.decode([Quiz].self, from: jsonData)
+        } catch {
+            NSLog("Error in getting data from local JSON file: \(error)")
+            // Handle the error appropriately, e.g., return an empty array
+            return []
+        }
+    }
 }
+
 
